f43fd999b80feecbc1dfcbb87ba9e0200b04dcf0
66616b65 <jesusstern@gmail.com>
2022-03-14 10:50:14 +0300

    comments

--- VPDelegates/MulticastDelegateExample.cs
+++ VPDelegates/MulticastDelegateExample.cs
@@ -8,18 +8,32 @@ namespace VPDelegates
 {
     public static class MulticastDelegateExample
     {
+        // Мультикаст делегат может ссылаться сразу на несколько методов
+        // Поэтому он не должен возвращать какое-либо значение
+        // И методы, на которые он ссылается, не должны
+        // Поэтому здесь мы пишем void
         public delegate void showDelegate(int a, int b);
         
+        // Метод с такой же сигнатурой, как у делегата
+        // В отличие от примера в MathDelegate, он не возвращает результат в виде числа
+        // А просто выводит значение в консоли
         public static void Add(int a, int b) => Console.WriteLine($"{a} + {b} = {a + b}");
 
+        // Аналогичный метод
         public static void Mul(int a, int b) => Console.WriteLine($"{a} * {b} = {a * b}");
         
+        // Метод для демонстрации
         public static void Example(int a, int b)
         {
-            showDelegate s = Add;
-            s += Mul;
+            // В переменную proc типа showDelegate записываем ссылку на метод Add
+            showDelegate proc = Add;
+            // Добавляем также ссылку на метод Mul
+            proc += Mul;
+            // А так можно удалить ссылку на метод Mul
             //s -= Mul;
-            s(a, b);
+            // Передаем в proc аргументы a и b
+            // Будут по очереди вызваны методы Add и Mul
+            proc(a, b);
         }
     }
 }
