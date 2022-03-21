83869658bc9d7504759bfaf799b48e71fcf6116d
66616b65 <jesusstern@gmail.com>
2022-03-21 21:16:13 +0300

    exceptions

---
+++ VPExceptions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VPExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GC.Collect();
+
+            //Example.Exception();
+            //Example.Demo();            
+            //Example.SpecialDemo();
+        }
+    }
+}
