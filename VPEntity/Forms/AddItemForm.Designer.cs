e2f3365a406590b14cdc194f3366c1c14ace2396
66616b65 <jesusstern@gmail.com>
2022-04-04 11:22:38 +0300

    entity template

---
+++ VPEntity/Forms/AddItemForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace VPEntity
+{
+    partial class AddItemForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtManufacturer = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblManufacturer = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(26, 48);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(183, 22);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(26, 110);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(183, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtManufacturer
+            // 
+            this.txtManufacturer.Location = new System.Drawing.Point(26, 172);
+            this.txtManufacturer.Name = "txtManufacturer";
+            this.txtManufacturer.Size = new System.Drawing.Size(183, 22);
+            this.txtManufacturer.TabIndex = 2;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(26, 233);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(183, 22);
+            this.txtPrice.TabIndex = 3;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(23, 28);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(62, 17);
+            this.lblId.TabIndex = 4;
+            this.lblId.Text = "Артикул";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(23, 90);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(106, 17);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Наименование";
+            // 
+            // lblManufacturer
+            // 
+            this.lblManufacturer.AutoSize = true;
+            this.lblManufacturer.Location = new System.Drawing.Point(23, 152);
+            this.lblManufacturer.Name = "lblManufacturer";
+            this.lblManufacturer.Size = new System.Drawing.Size(110, 17);
+            this.lblManufacturer.TabIndex = 6;
+            this.lblManufacturer.Text = "Производитель";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(23, 213);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(43, 17);
+            this.lblPrice.TabIndex = 7;
+            this.lblPrice.Text = "Цена";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Location = new System.Drawing.Point(329, 277);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(80, 30);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancel.Location = new System.Drawing.Point(428, 277);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(80, 30);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // AddItemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(530, 328);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblManufacturer);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtManufacturer);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Name = "AddItemForm";
+            this.Text = "Добавить новый товар";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtManufacturer;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblManufacturer;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
--- VPEntity/Forms/AddItemForm.cs
+++ VPEntity/Forms/AddItemForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VPEntity
 {
-    public partial class Form1 : Form
+    public partial class AddItemForm : Form
     {
-        public Form1()
+        public AddItemForm()
         {
             InitializeComponent();
         }
